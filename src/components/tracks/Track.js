820f5207cab150e00132d0d212176965c4e7c341
Alon Peleg <peleg619@gmail.com>
2019-08-24 18:24:04 +0300

    added button to go to the lyric page (not done yet)

--- src/components/tracks/Track.js
+++ src/components/tracks/Track.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Track = ({ track }) => {
   return (
@@ -17,6 +18,12 @@ const Track = ({ track }) => {
             </strong>
             : {track.album_name}
           </p>
+          <Link
+            to={`lyrics/track/${track.track_id}`}
+            className="btn btn-dark btn-block"
+          >
+            <i className="fas fa-chevron-right" /> View Lyrics
+          </Link>
         </div>
       </div>
     </div>
